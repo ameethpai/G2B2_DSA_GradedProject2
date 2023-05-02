5fde51f1af0ae0803ad167bd74e85662d69f4583
ameethpai <ameeth.pai@gmail.com>
2023-05-02 15:39:33 +0530

    Add files via upload

---
+++ SkyscraperConstruction1.java
@@ -0,0 +1,35 @@
+﻿import java.util.*;
+
+
+public class SkyscraperConstruction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the total number of floors in the building:");
+        int n = sc.nextInt();
+        
+        PriorityQueue<Integer> sizes = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter the floor size given on day " + i + ":");
+            int size = sc.nextInt();
+            
+            sizes.add(size);
+            
+            List<Integer> toBeAssembled = new ArrayList<>();
+            while (!sizes.isEmpty() && sizes.peek() == i) {
+                toBeAssembled.add(sizes.poll());
+            }
+            
+            if (toBeAssembled.isEmpty()) {
+                System.out.println();
+            } else {
+                for (int s : toBeAssembled) {
+                    System.out.print(s + " ");
+                }
+                System.out.println();
+            }
+        }
+        
+        sc.close();
+    }
+}
